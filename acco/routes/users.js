4bec1d4757f9d9017fa44c7df721f1eb074f6c29
muthuslm2006 <muthuslm2006@gmail.com>
2023-12-01 16:39:58 +0530

    roomtype

--- acco/routes/users.js
+++ acco/routes/users.js
@@ -96,6 +96,46 @@ router.post('/auth', async function (request, response) {
 //end multiple
 
 
+//st room type get
+
+router.get('/roomtype',function(req,res){
+  console.log("Welcome to room type");
+  try {
+    command = 'SELECT * FROM roomtype';
+    con.query(command, function (error, result) {
+      if (error) {
+        res.send({ "Message": "Unable to get Date " });
+      }
+      else {
+        res.send(result);
+      }
+    })
+  }
+  catch (e) {
+    console.log("Catch");
+    const statusCode = e.statusCoderes || 500;
+    res.status(statusCode, "Error").json({ success: 0, message: e.message, status: statusCode });
+  }
+  
+})
+
+
+//end room type get
+
+// st walkincf
+
+router.post('/walkincf',function(req,res){
+  console.log("Welcome to walk in current and future");
+  /*
+
+
+  */
+  cmd=""
+  res.send("Welcome to walk in current and future");
+})
+
+
+// end walkincf
 
 //otp gen st
 
