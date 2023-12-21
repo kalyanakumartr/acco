178bd928fdae54b6b44332da01667494339ef192
muthuslm2006 <98971383+muthuslm2006@users.noreply.github.com>
2023-12-21 16:58:16 +0530

    roomtype api

--- acco/app.js
+++ acco/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bookRouter=require('./routes/book');
 const router = require('./routes/users');
 
 var app = express();
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/book',bookRouter);
 // app.use(express.json());
 
 app.use(function(req, res) {
--- acco/routes/users.js
+++ acco/routes/users.js
@@ -212,32 +212,6 @@ router.post('/actualcheckin', authcheck, (req, res) => {
 
 
 
-//st room type get
-
-router.get('/roomtype', function (req, res) {
-  console.log("Welcome to room type");
-  try {
-    command = 'SELECT * FROM roomtype';
-    con.query(command, function (error, result) {
-      if (error) {
-        res.send({ "Message": "Unable to get Date " });
-      }
-      else {
-        res.send(result);
-      }
-    })
-  }
-  catch (e) {
-    console.log("Catch");
-    const statusCode = e.statusCoderes || 500;
-    res.status(statusCode, "Error").json({ success: 0, message: e.message, status: statusCode });
-  }
-
-})
-
-
-//end room type get
-
 
 
 //otp gen st
