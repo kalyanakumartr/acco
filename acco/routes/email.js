f5d7f9f22eb3ae26cd1d17df792ea8d139853d21
ananthee89 <ananthee89@gmail.com>
2024-05-20 11:53:52 +0530

    bug fixes in message otpverify
    bug fixes in message otpverify

--- acco/routes/email.js
+++ acco/routes/email.js
@@ -179,7 +179,7 @@ router.post('/verifyOTP', (req, res) => {
       con.query(cmmd, function (error, result) {
         console.log("count", result[0].counting);
         if (result[0].counting >= 1) {
-          res.send({ status: true, message: "Phonenumber verified successfully " });
+          res.send({ status: true, message: " OTP verified Successfully " });
         }
         else {
           console.log("Error", error)
